dfc7c7ec45addcf57fbcf56b62aa906bb5216e7a
Saurabh Patel <saurabh123815@gmail.com>
2022-06-14 14:20:17 +0530

    16 completed

--- pages/blog.js
+++ pages/blog.js
@@ -1,9 +1,40 @@
-import React from 'react'
+import React from "react";
+import stylesBlog from "../styles/blog.module.css";
+import styles from "../styles/Home.module.css";
+import Link from "next/link";
 
-const blog = () => {
+
+//  Step 1 : Collect all the files from blogdata directory
+//  Step 2 : Iterate through the and Display them
+const Blog = () => {
   return (
-    <div>blog</div>
-  )
-}
+    <>
+      <div className={styles.comtainer}>
+        <main className={styles.main}>
+          <div className={stylesBlog.blogItem}>
+            <Link href={"/blogpost/learn-javascript"}>
+              <h3 className={stylesBlog.blogheading}>
+                How to learn JavaScript in 2022?
+              </h3>
+            </Link>
+            <p>JavaScript is the language used to degign logic for the web</p>
+          </div>
+          <div className="blogItem">
+            <h3 className={stylesBlog.blogheading}>
+              How to learn JavaScript in 2022?
+            </h3>
+            <p>JavaScript is the language used to degign logic for the web</p>
+          </div>
+          <div className="blogItem">
+            <h3 className={stylesBlog.blogheading}>
+              How to learn JavaScript in 2022?
+            </h3>
+            <p>JavaScript is the language used to degign logic for the web</p>
+          </div>
+        </main>
+      </div>
+    </>
+  );
+};
 
-export default blog
+export default Blog;
--- pages/blogpost/[slug].js
+++ pages/blogpost/[slug].js
@@ -1,10 +1,39 @@
 import React from "react";
 import { useRouter } from "next/router";
+import styles from "../../styles/Home.module.css";
+
+
+//  Step 1 : Find the file corresponding to the slug
+//  Step 2 : Populate them inside the page
 
 const slug = () => {
+
   const router = useRouter();
   const { slug } = router.query;
-  return <div>{slug}</div>;
+  return (
+    <>
+      <style jsx>
+        {`
+        p{
+          max-width:900px;
+        }
+        `}
+      </style>
+      <div className={styles.comtainer}>
+        <main className={styles.main}>
+          <h1>Title of the page {slug}</h1>
+          <hr />
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facere non
+            ea perferendis voluptate officia at adipisci explicabo quos ratione.
+            Magni, quas debitis? Minima dicta esse quaerat vel. Quo vero dicta
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim alias magni sint sed eaque odit necessitatibus impedit debitis quis nulla. Et rem repellat dolorem dolorum aliquam saepe vitae iure dolore.
+            
+          </p>
+        </main>
+      </div>
+    </>
+  );
 };
 
 export default slug;
